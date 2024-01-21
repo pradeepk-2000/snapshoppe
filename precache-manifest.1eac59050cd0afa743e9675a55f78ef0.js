6ee58d4fc649f7ae4726269fd021136a4f5f8174
Pradeep K <pradeepksept2000@gmail.com>
2024-01-21 21:50:18 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.1eac59050cd0afa743e9675a55f78ef0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea59d0e78b82241f6a4f354a21df1b2",
+    "url": "/snapshoppe/index.html"
+  },
+  {
+    "revision": "e22ae8b6f921ed13447e",
+    "url": "/snapshoppe/static/css/main.a6b06ca1.chunk.css"
+  },
+  {
+    "revision": "162c3d4a158f0b168154",
+    "url": "/snapshoppe/static/js/2.fd828c15.chunk.js"
+  },
+  {
+    "revision": "e22ae8b6f921ed13447e",
+    "url": "/snapshoppe/static/js/main.3b6068e3.chunk.js"
+  },
+  {
+    "revision": "c88e743d715bbbf0a32d",
+    "url": "/snapshoppe/static/js/runtime~main.752578ae.js"
+  }
+]);
